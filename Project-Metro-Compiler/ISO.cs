9b54b63a26d7aaa02415e1a7485240f7d4871896
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-06-22 22:38:21 +0100

    Added ISO Template file.

---
+++ Project-Metro-Compiler/ISO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Metro_Compiler
+{
+    class ISO
+    {
+        //Use this to represent an ISO (see binso CD class). 
+        //It would be useful to represent each section as an item within a struct, OR a class.
+    }
+}
