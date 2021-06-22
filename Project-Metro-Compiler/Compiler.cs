a90c3338c8ee0c5902c5a785a9bb3a4b8bf923f9
Evulpes <54101182+Evulpes@users.noreply.github.com>
2021-06-22 22:36:55 +0100

    Added template classes

---
+++ Project-Metro-Compiler/Compiler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Metro_Compiler
+{
+    class Compiler
+    {
+        //Kris will convert the input file (.bin expected?
+        //Discussion about using direct .asm file if we can convert to bytecode, but likely overkill) into an ISO here.
+    }
+}
---
+++ Project-Metro-Compiler/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Metro_Compiler
+{
+    class Parser
+    {
+        //Need to ensure the input file meets ISO standards. This needs discussing further.
+    }
+}
